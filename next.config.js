3604cf02cb8d9e1f759acc521cd250974c6dd16d
rogeritael <rogeritael.contato@gmail.com>
2022-10-30 00:13:24 -0300

    css

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  pageExtensions: ['.tsx', '.ts', '.jsx', '.js']
+  swcMinify: true
 }
 
 module.exports = nextConfig
--- src/pages/games/index.tsx
+++ src/pages/games/index.tsx
@@ -4,7 +4,7 @@ import { Footer } from "../../components/Footer"
 import { GamesContainer } from "../../components/GamesContainer"
 import { Header } from "../../components/Header"
 import { ResultsNotFound } from "../../components/ResultsNotFound"
-import { Container } from "./gamesStyles"
+import { Container } from "../../styles/games-styles"
 
 export default function GamesPage(){
     const [ isResultsFound, SetIsResultsFound ] = useState(true);
