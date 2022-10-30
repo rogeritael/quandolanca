69913c001ecf57ae7496a6a3e0aea94658e55a02
rogeritael <rogeritael.contato@gmail.com>
2022-10-29 23:59:13 -0300

    teste deploy

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js']
+  pageExtensions: ['.tsx', '.ts', '.jsx', '.js']
 }
 
 module.exports = nextConfig
