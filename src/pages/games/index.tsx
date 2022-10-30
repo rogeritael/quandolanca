406a68a15e5e914550bbace89273aca957455d66
rogeritael <rogeritael.contato@gmail.com>
2022-10-29 23:36:56 -0300

    corrigindo bug em que o styles da página jogos não permitia a build do projeto

--- src/pages/games/index.tsx
+++ src/pages/games/index.tsx
@@ -1,10 +1,10 @@
-import { useState } from "react"
+import React, { useState } from "react"
 import { render } from "react-dom"
 import { Footer } from "../../components/Footer"
 import { GamesContainer } from "../../components/GamesContainer"
 import { Header } from "../../components/Header"
 import { ResultsNotFound } from "../../components/ResultsNotFound"
-import { Container } from "./styles"
+import { Container } from "../../styles/styles"
 
 export default function GamesPage(){
     const [ isResultsFound, SetIsResultsFound ] = useState(true);
